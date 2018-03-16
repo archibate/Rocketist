ccf58a625f5fea03d34265dc29a5fb3c2354e20d
彭于斌 <m17721388340@163.com>
2018-03-16 14:05:24 +0800

    Update Cylinder.h

--- Cylinder.h
+++ Cylinder.h
@@ -1,9 +1,9 @@
 class Cylinder {
 public:
-  virtual real_t getDiameter();
-  virtual real_t getRadius();
-  virtual real_t getHiratio();
-  virtual real_t getHeight();
+  virtual real_t getDiameter() const = 0;
+  virtual real_t getRadius() const = 0;
+  virtual real_t getHiratio() const = 0;
+  virtual real_t getHeight() const = 0;
 };
 
 class Cylinder_DH {
@@ -11,16 +11,16 @@ protected:
   real_t diameter;
   real_t hiratio;
 public:
-  virtual real_t getDiameter() {
+  virtual real_t getDiameter() const override final {
     return diameter;
   }
-  virtual real_t getRadius() {
+  virtual real_t getRadius() const override final {
     return getDiameter() / 2;
   }
-  virtual real_t getHiratio() {
+  virtual real_t getHiratio() const override final {
     return hiratio;
   }
-  virtual real_t getHeight() {
+  virtual real_t getHeight() const override final {
     return getRadius() * getHiratio();
   }
 };
