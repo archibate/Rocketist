5f995f8d05f4221bd308a1c4e820054e2a5db88c
彭于斌 <m17721388340@163.com>
2018-03-16 14:08:42 +0800

    Update FuelTank.h

--- FuelTank.h
+++ FuelTank.h
@@ -1,5 +1,26 @@
 class FuelTank {
 public:
-  virtual real_t getLiquidHydrogen();
-  virtual real_t getLiquidOxygen();
+  virtual real_t getLiquidHydrogen() const = 0;
+  virtual real_t getLiquidOxygen() const = 0;
+  virtual real_t costLiquidHydrogen(real_t t) = 0;
+  virtual real_t costLiquidOxygen(real_t t) = 0;
+};
+
+class FuelTank_HO : public FuelTank {
+protected:
+  real_t liquidOxygen;
+  real_t liquiqHydrogen;
+public:
+  virtual real_t getLiquidHydrogen() const override final {
+    return liquidOxygen;
+  }
+  virtual real_t getLiquidOxygen() const override final {
+    return liquidHydrogen;
+  }
+  virtual real_t costLiquidHydrogen(real_t t) override final {
+    return liquidHydrogen -= t;
+  }
+  virtual real_t costLiquidOxygen(real_t t) override final {
+    return liquidOxygen -= t;
+  }
 };
